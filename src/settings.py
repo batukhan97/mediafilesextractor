42c20dd3d651ea949471e9efc15be77427ebd7da
zhandos256 <r.zhandos256@gmail.com>
2022-04-07 12:13:06 +0600

    edit settings.py

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,17 @@ from colorama import Fore
 
 class Config:
     verison: float = 0.1
-    description: str = ''
-    logo: str = Fore.BLUE + ''''''
+    description: str = ''''''
+    logo: str = Fore.BLUE + """
+    █▀▄▀█ █▀▀ █▀▀▄ ░▀░ █▀▀█ 　 █▀▀▄ █▀▀█ ▀▀█▀▀ █▀▀█ 
+    █░▀░█ █▀▀ █░░█ ▀█▀ █▄▄█ 　 █░░█ █▄▄█ ░░█░░ █▄▄█ 
+    ▀░░░▀ ▀▀▀ ▀▀▀░ ▀▀▀ ▀░░▀ 　 ▀▀▀░ ▀░░▀ ░░▀░░ ▀░░▀ 
 
+    █▀▀ █░█ ▀▀█▀▀ █▀▀█ █▀▀█ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀█ 
+    █▀▀ ▄▀▄ ░░█░░ █▄▄▀ █▄▄█ █░░ ░░█░░ █░░█ █▄▄▀ 
+    ▀▀▀ ▀░▀ ░░▀░░ ▀░▀▀ ▀░░▀ ▀▀▀ ░░▀░░ ▀▀▀▀ ▀░▀▀
+    """
+
+    @staticmethod
+    def get_logo():
+        print(Config.logo)
