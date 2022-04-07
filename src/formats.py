711822fcacb2ce5ccdb990962f65f36b945c2866
zhandos256 <r.zhandos256@gmail.com>
2022-04-07 21:36:55 +0600

    add some formats

--- src/formats.py
+++ src/formats.py
@@ -1,5 +1,5 @@
 class Format:
     """ If your formats not here, just adding need format to attribute """
-    audio_format = ('mp3',)
-    video_format = ('mp4',)
-    pictures_format = ('jpg', 'jpeg', 'png', 'webp')
+    audio_format = ('mp3', 'opus', 'flac', 'aac', 'wav', 'wma')
+    video_format = ('mp4', 'webm', 'flv', 'vob', 'ogv', 'ogg', 'avi', 'wmv', 'm4p', 'm4v', '3gp')
+    pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng')
