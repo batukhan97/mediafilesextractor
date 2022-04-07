55032cfc7d4c236267d8a30a473ed388cf3f58d8
zhandos256 <r.zhandos256@gmail.com>
2022-04-07 13:01:57 +0600

    some edit formats.py

--- src/formats.py
+++ src/formats.py
@@ -1,4 +1,5 @@
 class Format:
+    """ If your formats not here, just adding need format to attribute """
     audio_format = ('mp3',)
     video_format = ('mp4',)
     pictures_format = ('jpg', 'jpeg', 'png', 'webp')
