2ee814a3ba55c43252cbe20dda8d57f8eb79e64d
zhandos256 <r.zhandos256@gmail.com>
2022-04-08 18:01:35 +0600

    add format instance

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ def main():
     """ Get path from user """
     path_to = Base.get_path()
 
+    """ Creating format instance"""
+    format = Format()
+
     """ Creating Base instances """
     audio_instance = Base('Audio', 'audio_folder')
     video_instance = Base('Video', 'video_folder')
     pictures_instance = Base('Image', 'images_folder')
 
     """ Adding media files if founding """
-    audio_instance.add_storage(path_to, Format.audio_format)
-    video_instance.add_storage(path_to, Format.video_format)
-    pictures_instance.add_storage(path_to, Format.pictures_format)
+    audio_instance.add_storage(path_to, format.audio_format)
+    video_instance.add_storage(path_to, format.video_format)
+    pictures_instance.add_storage(path_to, format.pictures_format)
 
     """ Check media files if found printing him """
     audio_instance.check_storage()
