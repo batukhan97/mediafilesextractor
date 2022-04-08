119ceb782662c961b91fa47aeddd3ac5a98aae3a
zhandos256 <r.zhandos256@gmail.com>
2022-04-08 17:11:17 +0600

    refactor formats, for core

--- src/formats.py
+++ src/formats.py
@@ -1,5 +1,6 @@
 class Format:
-    """ If your formats not here, just adding need format to attribute """
+    """ If your formats not here, just adding need format to attributes """
     audio_format = ('mp3', 'opus', 'flac', 'aac', 'wav', 'wma')
     video_format = ('mp4', 'webm', 'flv', 'vob', 'ogv', 'ogg', 'avi', 'wmv', 'm4p', 'm4v', '3gp')
     pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng')
+
