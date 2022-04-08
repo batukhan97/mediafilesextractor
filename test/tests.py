ff090c23510aa6dd26ba05b9b5585a94358a7984
zhandos256 <r.zhandos256@gmail.com>
2022-04-08 17:18:49 +0600

    init

---
+++ test/tests.py
@@ -0,0 +1,13 @@
+import unittest
+from src.core import Base
+from src.formats import Format
+
+
+class TestBase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.base = Base
+        self.storage = []
+        self.format = Format
+
+    def test_add_storage(self):
+        pass
