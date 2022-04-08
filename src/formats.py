e66c0105a019ebb5dd998455e159c32c746e12e2
zhandos256 <r.zhandos256@gmail.com>
2022-04-08 20:53:14 +0600

    some edit formats.py

--- src/formats.py
+++ src/formats.py
@@ -1,6 +1,6 @@
 class Format:
-    """ If your formats not here, just adding need format to attributes """
     def __init__(self):
+        """ Add here you needed formats here """
         self.audio_format = ('mp3', 'opus', 'flac', 'aac', 'wav', 'wma')
         self.video_format = ('mp4', 'webm', 'flv', 'vob', 'ogv', 'ogg', 'avi', 'wmv', 'm4p', 'm4v', '3gp')
         self.pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng')
