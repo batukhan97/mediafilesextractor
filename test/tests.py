1183eca48ebd1fa1db9258064dddcce012b18ef6
zhandos256 <r.zhandos256@gmail.com>
2022-04-08 17:57:31 +0600

    some edit

--- test/tests.py
+++ test/tests.py
@@ -1,17 +1,5 @@
 import unittest
-from src.core import Base
-from src.formats import Format
 
 
 class TestBase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.base = Base
-        self.storage = {}
-        self.format = Format
-
-    def test_add_storage(self):
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
+    pass
