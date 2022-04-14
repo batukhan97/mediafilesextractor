6d721dfc6c2c4ebf1e680070c923e5733ebf5457
zhandos256 <r.zhandos256@gmail.com>
2022-04-14 09:58:32 +0600

    update videos formats

--- src/formats.py
+++ src/formats.py
@@ -2,5 +2,5 @@ class Format:
     def __init__(self):
         """ Add here you needed formats here """
         self.audio_format = ('mp3', 'opus', 'flac', 'aac', 'wav', 'wma', 'alac', 'ogg', 'aiff', 'pcm', 'dsd')
-        self.video_format = ('mp4', 'webm', 'flv', 'vob', 'ogv', 'ogg', 'avi', 'wmv', 'm4p', 'm4v', '3gp')
-        self.pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng', 'tiff', 'raw', 'ico')
+        self.video_format = ('mov', 'swf', 'mp4', 'mkv', 'flv', 'wmv', 'avi', '3gp', 'vob', 'aaf', 'mod', 'mpeg')
+        self.pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng', 'tiff', 'raw', 'ico',)
