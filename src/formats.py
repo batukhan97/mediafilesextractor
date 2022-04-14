1b0cc26d23c942237100733f0a3882b39d719e47
zhandos256 <r.zhandos256@gmail.com>
2022-04-14 09:41:38 +0600

    add image formats: tiff, raw, ico

--- src/formats.py
+++ src/formats.py
@@ -3,4 +3,4 @@ class Format:
         """ Add here you needed formats here """
         self.audio_format = ('mp3', 'opus', 'flac', 'aac', 'wav', 'wma', 'alac', 'ogg', 'aiff', 'pcm', 'dsd')
         self.video_format = ('mp4', 'webm', 'flv', 'vob', 'ogv', 'ogg', 'avi', 'wmv', 'm4p', 'm4v', '3gp')
-        self.pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng')
+        self.pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng', 'tiff', 'raw', 'ico')
