cf5f235553a0921ab64fa65e7b3abc587ffbd42a
zhandos256 <r.zhandos256@gmail.com>
2022-04-14 10:01:48 +0600

    update image formats

--- src/formats.py
+++ src/formats.py
@@ -3,4 +3,4 @@ class Format:
         """ Add here you needed formats here """
         self.audio_format = ('mp3', 'opus', 'flac', 'aac', 'wav', 'wma', 'alac', 'ogg', 'aiff', 'pcm', 'dsd')
         self.video_format = ('mov', 'swf', 'mp4', 'mkv', 'flv', 'wmv', 'avi', '3gp', 'vob', 'aaf', 'mod', 'mpeg')
-        self.pictures_format = ('jpg', 'jpeg', 'png', 'webp', 'gif', 'svg', 'avif', 'apng', 'tiff', 'raw', 'ico',)
+        self.pictures_format = ('mbp', 'jpg', 'png', 'raw', 'psd', 'eps', 'gif', 'ico', 'ai', 'svg', 'tif', 'cdr')
