2764118183b7742a72ab6e4dba9e25ccc780a124
zhandos256 <r.zhandos256@gmail.com>
2022-04-16 10:37:57 +0600

    add some audio formats

--- src/formats.py
+++ src/formats.py
@@ -1,6 +1,6 @@
 class Format:
     def __init__(self):
         """ Add here you needed formats here """
-        self.audio_format = ('wav', 'flac', 'mp3', 'ogg', 'm3u', 'acc', 'wma', 'midi', 'aif', 'm4a', 'm4a', 'mpa', 'pls')
+        self.audio_format = ('wav', 'flac', 'mp3', 'ogg', 'm3u', 'acc', 'wma', 'midi', 'aif', 'm4a', 'm4a', 'mpa', 'pls', 'shn', 'ape', 'wv', 'tta', 'spx')
         self.video_format = ('mov', 'swf', 'mp4', 'mkv', 'flv', 'wmv', 'avi', '3gp', 'vob', 'aaf', 'mod', 'mpeg')
         self.pictures_format = ('mbp', 'jpg', 'png', 'raw', 'psd', 'eps', 'gif', 'ico', 'ai', 'svg', 'tif', 'cdr')
