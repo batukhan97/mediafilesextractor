aeca3ea4948405feac6e9ce97b17f3ee338c6882
zhandos256 <r.zhandos256@gmail.com>
2022-04-20 14:14:44 +0600

    adding media attributes to class Base

--- src/base.py
+++ src/base.py
@@ -4,6 +4,15 @@ from shutil import copy2
 
 
 class Base:
+    audio_format = (
+    '.wav', '.flac', '.mp3', '.ogg', '.m3u', '.acc', '.wma', '.midi', '.aif', '.m4a', '.m4a', '.mpa',
+    '.pls', '.shn', '.ape', '.wv', '.tta', '.spx')
+    video_format = (
+    '.mov', '.swf', '.mp4', '.mkv', '.flv', '.wmv', '.avi', '.3gp', '.vob', '.aaf', '.mod', '.mpeg',
+    '.webm', '.f4v', '.swf')
+    pictures_format = (
+    '.mbp', '.jpg', '.png', '.raw', '.psd', '.eps', '.gif', '.ico', '.ai', '.svg', '.tif', '.cdr')
+
     """ Script help you automatically extract your media files from folders """
     def __init__(self, name, folder_name):
         self.name = name
