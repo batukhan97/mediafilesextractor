58d21a945825b8f62597afee7a5b717942fa128d
zhandos256 <r.zhandos256@gmail.com>
2022-04-20 14:14:05 +0600

    delete format instance and change argument in add_storage fucntion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from src.base import Base
-from src.formats import Format
 from src.settings import Config
 
 
@@ -10,18 +9,15 @@ def main():
     """ Get path from user """
     path_to = Base.get_path()
 
-    """ Creating format instance"""
-    format = Format()
-
     """ Creating Base instances """
     audio_instance = Base('Audios', 'audio_folder')
     video_instance = Base('Videos', 'video_folder')
     pictures_instance = Base('Pictures', 'images_folder')
 
     """ Adding media files if founding """
-    audio_instance.add_storage(path_to, format.audio_format)
-    video_instance.add_storage(path_to, format.video_format)
-    pictures_instance.add_storage(path_to, format.pictures_format)
+    audio_instance.add_storage(path_to, Base.audio_format)
+    video_instance.add_storage(path_to, Base.video_format)
+    pictures_instance.add_storage(path_to, Base.pictures_format)
 
     """ Extracting media files, each instance is responsible for its own format """
     audio_instance.extract_files()
